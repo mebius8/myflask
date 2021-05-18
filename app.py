be16914429023dcc17e87224f053f0a40382895d
fushaoshan <317024297@qq.com>
2021-05-18 11:33:45 +0800

    第一次提交吧

---
+++ app.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author： fushaoshan
+# datetime： 2021/5/18 11:32 上午
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Welcome to My Watchlist!'
